9e38e4ac64857eed2dad819eff57d4550dd293fa
ki-Ubuntu-dev <byeongkicho@gmail.com>
2020-09-06 02:02:04 +0900

    clone build done w book

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>About this page: I built it because I love movies.</span>
+      <span>George Orweell</span>
+    </div>
+  );
+}
+
+export default About;
